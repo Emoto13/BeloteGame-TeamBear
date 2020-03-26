10d5f07396413bba9126cc26ad0df3b0b4ce45b2
Emoto13 <emcho_spassov@abv.bg>
2020-03-26 20:57:23 +0200

    Fixed typos

--- Team.py
+++ Team.py
@@ -1,40 +1,47 @@
 from Player import Player
-from announcements import quintes, quartes, trieces, power_of_cards
+from announcements import power_of_cards
+
 
 class Team:
 
-	def __init__(self, teamname = 'Team', player1 = None, player2 = None):
-		self.teamname = teamname
-		self.player1 = player1
-		self.player2 = player2
+    def __init__(self, team_name='Team', player1=None, player2=None):
+        self.team_name = team_name
+        self.player1 = player1
+        self.player2 = player2
 
-	def get_best_declaration(self):
-		p1_best_anou = self.player1.best_announcement()
-		p2_best_anou = self.player2.best_announcement()
-		if len(p1_best_anou) > len(p2_best_anou):
-			return p1_best_anou
-		elif len(p1_best_anou) == len(p2_best_anou):
-			return Team.check_highest_card(p1_best_anou, p2_best_anou)
-		else:
-			return p2_best_anou
+    def get_best_declaration(self):
+        player1_best_announcement = self.player1.best_announcement()
+        player2_best_announcement = self.player2.best_announcement()
 
-	@staticmethod
-	def check_highest_card(p1_best_anou, p2_best_anou):
-		p1_higest_card = power_of_cards[p1_best_anou[-1][:-1]]  #gets the power of the card and converts it to int so we can ><
-		p2_higest_card = power_of_cards[p2_best_anou[-1][:-1]]  
-		if p1_higest_card > p2_higest_card:
-			return p1_best_anou
-		else:
-			return p2_best_anou
+        if len(player1_best_announcement) > len(player2_best_announcement):
+            return player1_best_announcement
+
+        elif len(player1_best_announcement) == len(player2_best_announcement):
+            return Team.check_highest_card(player1_best_announcement, player2_best_announcement)
+
+        else:
+            return player2_best_announcement
+
+    @staticmethod
+    def check_highest_card(player1_best_announcement, player2_best_announcement):
+        # gets the power of the card and converts it to int so we can >=<
+        p1_highest_card = power_of_cards[player1_best_announcement[-1][:-1]]
+        p2_highest_card = power_of_cards[player2_best_announcement[-1][:-1]]
+
+        if p1_highest_card > p2_highest_card:
+            return player1_best_announcement
+        else:
+            return player2_best_announcement
+
 
 def main():
-	player1 = Player(name = 'Gosho')
-	player2 = Player(name = 'Pesho')
-	player1.set_hand(['10s', '10c', 'Kh', 'Qh', '10d', '10h', 'Ah'])
-	player2.set_hand(['10s', '10c', 'Kc', 'Qc', '10d', '10h', 'Jc'])
-	tm = Team('Mechkite', player1, player2)
-	tm.get_best_declaration()
-	# print(tm.player2.best_announcement())
+    player1 = Player(name='Gosho')
+    player2 = Player(name='Pesho')
+    player1.set_hand(['10s', '10c', 'Kh', 'Qh', '10d', '10h', 'Ah'])
+    player2.set_hand(['10s', '10c', 'Kc', 'Qc', '10d', '10h', 'Jc'])
+    tm = Team('Mechkite', player1, player2)
+    tm.get_best_declaration()
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- test_class_Team.py
+++ test_class_Team.py
@@ -2,16 +2,10 @@ import unittest
 from Team import Team
 
 
-
 class TestTeam(unittest.TestCase):
-
-	def test_if_constructor_sets_attr_correctly(self):
-		tm = Team()
-		
-
-
-
+    def test_if_constructor_sets_attr_correctly(self):
+        pass
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
