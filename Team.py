6f9da2f28a851e52bf1af58bc58943342285206d
Emoto13 <emcho_spassov@abv.bg>
2020-03-26 22:44:39 +0200

    Add unit tests and fix typos

--- Team.py
+++ Team.py
@@ -15,25 +15,22 @@ class Team:
         player1_best_announcement = self.player1.best_announcement()
         player2_best_announcement = self.player2.best_announcement()
 
-        if len(player1_best_announcement) > len(player2_best_announcement):
-            return player1_best_announcement
-
-        elif len(player1_best_announcement) == len(player2_best_announcement):
+        if len(player1_best_announcement) == len(player2_best_announcement):
             return Team.check_highest_card(player1_best_announcement, player2_best_announcement)
 
-        else:
-            return player2_best_announcement
+        if len(player1_best_announcement) > len(player2_best_announcement):
+            return player1_best_announcement
+        return player2_best_announcement
 
     @staticmethod
     def check_highest_card(player1_best_announcement, player2_best_announcement):
         # gets the power of the card and converts it to int so we can >=<
-        p1_highest_card = power_of_cards[player1_best_announcement[-1][:-1]]
-        p2_highest_card = power_of_cards[player2_best_announcement[-1][:-1]]
+        player1_highest_card = power_of_cards[player1_best_announcement[-1][:-1]]
+        player2_highest_card = power_of_cards[player2_best_announcement[-1][:-1]]
 
-        if p1_highest_card > p2_highest_card:
+        if player1_highest_card > player2_highest_card:
             return player1_best_announcement
-        else:
-            return player2_best_announcement
+        return player2_best_announcement
 
     def to_dict(self):
         dicts = {
--- test_class_Team.py
+++ test_class_Team.py
@@ -3,9 +3,29 @@ from Team import Team
 
 
 class TestTeam(unittest.TestCase):
-    def test_if_constructor_sets_attr_correctly(self):
+    def test_if_constructor_sets_attributes_correctly(self):
         pass
 
+    def test_if_get_best_declaration_works_correctly_for_different_declarations(self):
+        pass
+
+    def test_if_get_best_declaration_works_correctly_for_declarations_of_the_same_type(self):
+        pass
+
+    def test_if_check_highest_cards_works_for_picture_cards(self):
+        pass
+
+    def test_if_check_highest_cards_works_for_number_cards(self):
+        pass
+
+    def test_if_check_highest_cards_works_for_picture_and_number_cards(self):
+        pass
+
+    def test_if_to_dict_works_correctly(self):
+        pass
+
+    def test_if_to_json_works_correctly(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
