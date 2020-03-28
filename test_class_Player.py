cc93142e2269de162f51f3bd7438fa49c8edc0ba
Emoto13 <emcho_spassov@abv.bg>
2020-03-28 09:07:15 +0200

    Add unit tests

--- test_class_Player.py
+++ test_class_Player.py
@@ -62,28 +62,115 @@ class TestPlayerClass(unittest.TestCase):
         self.assertEqual(pl.carres, [])
 
     # add edge cases to this test
-    def test_if_check_if_hand_contains_quintes_works_correctly(self):
-        pass
+    def test_if_check_if_hand_contains_quintes_works_correctly_when_there_is_a_quinte(self):
+        player = Player()
+        res = player.check_if_hand_contains_quintes({'10h', 'Jh', 'Qh', 'Kh', 'Ah'})
+        exp = set()
+
+        self.assertEqual(res, exp)
+        self.assertEqual(player.quintes, [['10h', 'Jh', 'Qh', 'Kh', 'Ah']])
+
+    def test_if_check_if_hand_contains_quintes_works_correctly_when_there_is_no_quinte(self):
+        player = Player()
+        res = player.check_if_hand_contains_quintes({'10h', 'Jh', 'Qc', 'Kc', 'As'})
+        exp = {'10h', 'Jh', 'Qc', 'Kc', 'As'}
+
+        self.assertEqual(res, exp)
+        self.assertEqual(player.quintes, [])
 
     # add edge cases to this test
-    def test_if_check_if_hand_contains_quartes_works_correctly(self):
-        pass
+    def test_if_check_if_hand_contains_quartes_works_correctly_when_there_is_a_quarte(self):
+        player = Player()
+        res = player.check_if_hand_contains_quartes({'10c', 'Jc', 'Qc', 'Kc'})
+        exp = set()
+
+        self.assertEqual(res, exp)
+        self.assertEqual(player.quartes, [['10c', 'Jc', 'Qc', 'Kc']])
+
+    def test_if_check_if_hand_contains_quartes_works_correctly_when_there_is_not_quarte(self):
+        player = Player()
+        res = player.check_if_hand_contains_quartes({'10c', 'Jc', 'Qc', 'Kh'})
+        exp = {'10c', 'Jc', 'Qc', 'Kh'}
+
+        self.assertEqual(res, exp)
+        self.assertEqual(player.quartes, [])
+
+    def test_if_check_if_hand_contains_quartes_works_correctly_when_there_are_multiple_quartes(self):
+        player = Player()
+        res = player.check_if_hand_contains_quartes({'10c', 'Jc', 'Qc', 'Kc', '10h', 'Jh', 'Qh', 'Kh'})
+        exp = set()
+
+        self.assertEqual(res, exp)
+        self.assertEqual(player.quartes, [['10c', 'Jc', 'Qc', 'Kc'], ['10h', 'Jh', 'Qh', 'Kh']])
 
     # add edge cases to this test
-    def test_if_check_if_hand_contains_trieces_works_correctly(self):
-        pass
+    def test_if_check_if_hand_contains_trieces_works_correctly_when_there_is_a_triece(self):
+        player = Player()
+        res = player.check_if_hand_contains_trieces({'10c', 'Jc', 'Qc'})
+        exp = set()
+
+        self.assertEqual(res, exp)
+        self.assertEqual(player.trieces, [['10c', 'Jc', 'Qc']])
+
+    def test_if_check_if_hand_contains_trieces_works_correctly_when_there_is_not_triece(self):
+        player = Player()
+        res = player.check_if_hand_contains_trieces({'10c', 'Jc', 'Qh', 'Kh'})
+        exp = {'10c', 'Jc', 'Qh', 'Kh'}
+
+        self.assertEqual(res, exp)
+        self.assertEqual(player.trieces, [])
+
+    def test_if_check_if_hand_contains_trieces_works_correctly_when_there_are_multiple_trieces_triece(self):
+        player = Player()
+        res = player.check_if_hand_contains_trieces({'10c', 'Jc', 'Qc', 'Jh', 'Qh', 'Kh'})
+        exp = set()
+
+        self.assertEqual(res, exp)
+        self.assertEqual(player.trieces, [['Jh', 'Qh', 'Kh'], ['10c', 'Jc', 'Qc']])
 
     # add edge cases to this test
-    def test_if_get_announcements_works_correctly(self):
-        pass
+    def test_if_get_announcements_works_correctly_when_there_are_different_announcements(self):
+        player = Player()
+        player.set_hand(['10c', 'Jc', 'Qc', '10h', 'Jh', 'Qh', 'Kh'])
+        res = ['belot', 'quarte', 'triece']
+
+        self.assertEqual(res, player.get_announcements())
+
+    def test_if_get_announcements_works_correctly_when_there_are_announcements_of_same_type(self):
+        player = Player()
+        player.set_hand(['10c', 'Jc', 'Qc', 'Jh', 'Qh', 'Kh'])
+        res = ['belot', 'triece', 'triece']
+
+        self.assertEqual(res, player.get_announcements())
 
     # add edge cases to this test
-    def test_if_best_declaration_works_correctly(self):
-        pass
+    def test_if_best_declaration_works_correctly_when_all_announcements_are_different(self):
+        player = Player()
+        player.set_hand((['10c', 'Jc', 'Qc', 'Kc', 'Qh', 'Kh', 'Ah']))
+        res = ['10c', 'Jc', 'Qc', 'Kc']
+        self.assertEqual(res, player.best_announcement())
+
+    def test_if_best_declaration_works_correctly_when_there_are_announcements_of_the_same_type(self):
+        player = Player()
+        player.set_hand((['10c', 'Jc', 'Qc', 'Kc', 'Jh', 'Qh', 'Kh', 'Ah']))
+        res = ['Jh', 'Qh', 'Kh', 'Ah']
+
+        self.assertEqual(res, player.best_announcement())
+
+        # add edge cases to this test
+    def test_if_clear_points_works_correctly(self):
+        player = Player()
+        player.set_hand(['10c', 'Jc', 'Qc', 'Kc', 'Jh', 'Qh', 'Kh', 'Ah'])
+        player.clear_announcements()
+        res = ['belot', 'belot']
+        self.assertEqual(res, player.get_announcements())
 
     # add edge cases to this test
     def test_if_get_points_works_correctly(self):
-        pass
+        player = Player()
+        player.set_hand(['10c', 'Jc', 'Qc', 'Kc', 'Jh', 'Qh', 'Kh', 'Ah'])
+        points = 140
+        self.assertEqual(points, player.get_points())
 
     # add edge cases to this test
     def test_if_to_json_works_correctly(self):
