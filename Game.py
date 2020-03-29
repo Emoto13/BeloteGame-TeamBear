3499435a68ce59192d7af82e25cbc7a188b742f1
Nikolai Kasev <n.kasev98@gmail.com>
2020-03-29 13:31:02 +0300

    Game Refactor

--- Game.py
+++ Game.py
@@ -3,40 +3,42 @@ from Team import Team
 from Round import Round
 from utils import game_end
 from cards import cards
+from game_modes import game_modes
 from WriteToTxt import WriteToTxt
 import random
 
 
 class Game:
-    def __init__(self, game_id: int, team1: Team = None, team2: Team = None):  # takes teams
+    def __init__(self, game_id: int, team1: Team = None, team2: Team = None,
+                 write_to_txt: WriteToTxt = None):  # takes teams
         self.id = f'game : {game_id}'
         self.team1 = team1
         self.team2 = team2
+        self.write_to_txt = write_to_txt
 
     def play_game(self):
         round_id = 1
-        write_to_txt = WriteToTxt()
-        write_to_txt.setting_up_txt_teams(self.team1, self.team2)
-
+        
         while not game_end(self.team1.team_points, self.team2.team_points):
-            self.set_player_hands()
+            self.set_game()
 
             r = Round(round_id, self.team1, self.team2)
             r.compare_best_announcements()
 
-            write_to_txt.write_results(self.team1.team_points, self.team2.team_points, round_id)
+            self.write_to_txt.write_results(self.team1.team_points, self.team2.team_points, round_id)
 
             r.clear_announcements_for_round()
             round_id += 1
 
-        write_to_txt.write_game_end_output(self.team1.team_points, self.team2.team_points)
+        self.write_to_txt.write_game_end_output(self.team1.team_points, self.team2.team_points)
 
-    def set_player_hands(self):
+    def set_game(self):
+        game_mode = random.choice(game_modes)
         random.shuffle(cards)
-        self.team1.player1.set_hand(cards[:8])
-        self.team1.player2.set_hand(cards[8:16])
-        self.team2.player1.set_hand(cards[16:24])
-        self.team2.player2.set_hand(cards[24:])
+        self.team1.player1.set_hand(cards[:8], game_mode)
+        self.team1.player2.set_hand(cards[8:16], game_mode)
+        self.team2.player1.set_hand(cards[16:24], game_mode)
+        self.team2.player2.set_hand(cards[24:], game_mode)
 
     # @staticmethod
     # def to_dict(current_round):
@@ -63,8 +65,8 @@ def main():
     player3 = Player(name='Gosho')
     player4 = Player(name='Pesho')
     team2 = Team(team_name='Kotetata', player1=player3, player2=player4)
-    g = Game(game_id=1, team1=team1, team2=team2)
-
+    w = WriteToTxt(team1,team2)
+    g = Game(game_id=1, team1=team1, team2=team2, write_to_txt = w)
     g.play_game()
     # print(team2.player1.get_announcements())
     # print(team2.player2.get_announcements())
