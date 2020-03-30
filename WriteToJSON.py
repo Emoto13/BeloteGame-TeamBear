ef4bffbe3ec2317ed3e3c672aa470e90bf874fa8
Emoto13 <emcho_spassov@abv.bg>
2020-03-30 11:36:25 +0300

    Create WriteToJSON class

---
+++ WriteToJSON.py
@@ -0,0 +1,19 @@
+from pretty_json import prettyjson
+from utils import format_json
+
+
+class WriteToJSON:
+    def __init__(self):
+        self.games = {}
+
+    def add_game(self, game_id, dicts):
+        self.games[game_id] = dicts
+
+    def to_json(self):
+        json_repr = prettyjson(self.games)
+        json_repr = format_json(json_repr)
+        return json_repr
+
+    def __del__(self):
+        with open('data.json', 'w') as file:
+            file.write(self.to_json())
