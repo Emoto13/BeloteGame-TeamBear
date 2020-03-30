8365d91a875a94e2a7e9e75913ca46267b2593a1
Emoto13 <emcho_spassov@abv.bg>
2020-03-30 11:37:57 +0300

    Moved function is_match_won to utils and added write to json logic

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 from Player import Player
 from Team import Team
-from Round import Round
+from utils import is_match_won
 from Game import Game
 from WriteToTxt import WriteToTxt
+from WriteToJSON import WriteToJSON
 
 
 def input_data():
@@ -11,24 +12,20 @@ def input_data():
     # team2 = input('Team 2 name: ')
     team1 = 'Mechkite'
     team2 = 'Kotkite'
-    teams = [team1,team2]
+    teams = [team1, team2]
 
     # players_team1 = input(f'"{team1}" players: ')
     # players_team2 = input(f'"{team2}" players: ')
     # players_team1 = players_team1.split(',')
     # players_team2 = players_team2.split(',')
-    players_team1 = ['Marto','Rado']
-    players_team2 = ['Gosho','Pesho']
+    players_team1 = ['Marto', 'Rado']
+    players_team2 = ['Gosho', 'Pesho']
     players = players_team1 + players_team2
-    
-    data = [teams,players]
+
+    data = [teams, players]
 
     return data
 
-def check_for_match_end(team1_points, team2_points):
-    if team1_points == 2 or team2_points == 2:
-        return False
-    return True
 
 def main():
     game_id = 1
@@ -43,10 +40,11 @@ def main():
     player4 = Player(players[3])
     team2 = Team(teams[1], player3, player4)
 
-    w = WriteToTxt(team1, team2)
- 
-    while check_for_match_end(team1.games_won, team2.games_won): 
-        g = Game(game_id, team1, team2, w)
+    write_to_txt = WriteToTxt(team1, team2)
+    write_to_json = WriteToJSON()
+
+    while not is_match_won(team1.games_won, team2.games_won):
+        g = Game(game_id=game_id, team1=team1, team2=team2, write_to_txt=write_to_txt, write_to_json=write_to_json)
         g.play_game()
         game_id += 1
 
